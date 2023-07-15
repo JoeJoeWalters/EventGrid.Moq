503ca2ec423044dd595599843a0262922693f000
Joe Walters <info@thenakeddeveloper.com>
2023-07-15 21:22:09 +0100

    Initial Hack

---
+++ ProcessData.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Messaging.EventGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventGrid.Moq
+{
+    public class ProcessData
+    {
+        private EventGridPublisherClient _publisherClient;
+
+        public ProcessData(EventGridPublisherClient publisherClient) 
+        {
+            _publisherClient = publisherClient;
+        }
+
+        public async Task Run()
+        {
+            await _publisherClient.SendEventAsync(new EventGridEvent("subject", "type", "1.1", new BinaryData("")), new CancellationToken());
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure;
+using Azure.Messaging.EventGrid;
+using EventGrid.Moq;
+using Moq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+// Create some storage to analyse the events
+List<EventGridEvent> events = new List<EventGridEvent>();
+
+// Mock the event grid and response
+Mock<EventGridPublisherClient> mockClient = new();
+Mock<Response> mockResponse = new();
+mockResponse.SetupGet(x => x.Status).Returns((int)HttpStatusCode.OK);
+mockResponse.SetupGet(x => x.Content).Returns(BinaryData.FromString(""));
+
+// Tell the mock to store the data in the list
+mockClient.Setup(c => c.SendEventAsync(It.IsAny<EventGridEvent>(), It.IsAny<CancellationToken>()))
+    .Callback((EventGridEvent ev, CancellationToken ct) => 
+        { 
+            events.Add(ev); // Or call an actual endpoint if we want to simulate that for integration purposes
+        })
+    .ReturnsAsync(mockResponse.Object);
+
+// Send the mock to the class instead of the real thing
+ProcessData procesData = new ProcessData(mockClient.Object);
+
+// Start processing
+await procesData.Run();
+
+// Wait!
+foreach (EventGridEvent ev in events)
+{
+    Console.WriteLine(ev.Data.ToString());
+}
+
+Console.ReadLine();
+
+
